6d9bd7bd8857bdeb264fb2f6f48bff82cfb2f026
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-03-07 14:27:50 +0100

    Add files via upload

---
+++ RuntimeLoader/RuntimeLoader64/RuntimeLoader64.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "tchar.h"
+#include <metahost.h>
+
+#import "mscorlib.tlb" \
+	raw_interfaces_only \
+    high_property_prefixes("_get", "_put", "_putref") \
+    rename("ReportEvent", "InteropServices_ReportEvent") \
+	exclude("ITrackingHandler")
+
+#pragma comment(lib,"mscoree.lib")
+
+using namespace mscorlib;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved)
+{
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) HRESULT __stdcall LoadRuntime(ICorRuntimeHost* &pCorRuntimeHost)
+{
+	ICLRMetaHost* pMetaHost = nullptr;
+	ICLRRuntimeInfo* pRuntimeInfo = nullptr;
+	BOOL isLoadable = FALSE;
+
+	HRESULT hr = CLRCreateInstance(CLSID_CLRMetaHost, IID_PPV_ARGS(&pMetaHost));
+  
+	if (FAILED(hr))
+        goto Cleanup;
+
+	hr = pMetaHost->GetRuntime(_T("v4.0.30319"), IID_PPV_ARGS(&pRuntimeInfo));
+
+    if (FAILED(hr))
+        goto Cleanup;
+
+    hr = pRuntimeInfo->IsLoadable(&isLoadable);
+
+    if (FAILED(hr))
+        goto Cleanup;
+
+    if (!isLoadable)
+    {
+		hr = E_FAIL;
+        goto Cleanup;
+    }
+	
+	hr = pRuntimeInfo->GetInterface(CLSID_CorRuntimeHost, IID_PPV_ARGS(&pCorRuntimeHost));
+
+	Cleanup:
+
+    if (pMetaHost)
+    {
+        pMetaHost->Release();
+        pMetaHost = nullptr;
+    }
+
+    if (pRuntimeInfo)
+    {
+        pRuntimeInfo->Release();
+        pRuntimeInfo = nullptr;
+    }
+
+	return hr;
+}
