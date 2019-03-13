da1b9fa5c3b0addd4f3dce15f813a968714c9c49
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-03-13 18:04:46 +0100

    Update RuntimeLoader64.cpp

--- RuntimeLoader/RuntimeLoader64/RuntimeLoader64.cpp
+++ RuntimeLoader/RuntimeLoader64/RuntimeLoader64.cpp
@@ -3,10 +3,10 @@
 #include <metahost.h>
 
 #import "mscorlib.tlb" \
-	raw_interfaces_only \
+    raw_interfaces_only \
     high_property_prefixes("_get", "_put", "_putref") \
     rename("ReportEvent", "InteropServices_ReportEvent") \
-	exclude("ITrackingHandler")
+    exclude("ITrackingHandler")
 
 #pragma comment(lib,"mscoree.lib")
 
